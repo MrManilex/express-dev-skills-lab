52a2841aa6293a2319c0522bc8b564f3f0e86034
WangManLex <alexandermaniwang99@gmail.com>
2021-10-27 22:35:45 -0400

    rearrange files

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,7 +2,7 @@ import * as skillsDb from '../data/dev-skills-db.js'
 
 function index(req, res){
   skillsDb.find({}, function(err, skills){
-    res.render('skills', {
+    res.render('skills/index', {
       skills,
       err
     })
