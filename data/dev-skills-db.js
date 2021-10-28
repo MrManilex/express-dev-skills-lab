17333ac63f832f1aba12a8c8092d6b42559f778e
WangManLex <alexandermaniwang99@gmail.com>
2021-10-27 22:01:31 -0400

    rename users to skills

--- data/dev-skills-db.js
+++ data/dev-skills-db.js
@@ -1,4 +1,4 @@
-const devs = [
+const skills = [
   {text: 'Feed llama', done: true, _id: 125223},
   {text: 'Sleep under the stars', done: false, _id: 127904},
   {text: 'Buy milk', done: false, _id: 139608},
